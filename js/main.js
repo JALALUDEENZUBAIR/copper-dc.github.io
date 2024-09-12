f841a3a078d83c180cb7474a510be139cea9ddf3
JALALUDEENZUBAIR <jalaludeenzubair15@gmail.com>
2024-09-12 10:18:41 +0530

    nav has been changed

--- js/main.js
+++ js/main.js
@@ -3,5 +3,4 @@ const toggle = () => {
     var nav = document.getElementById("list");
     nav.classList.toggle("show");
 }
-
 document.getElementById("toggle-btn").addEventListener("click",toggle);
